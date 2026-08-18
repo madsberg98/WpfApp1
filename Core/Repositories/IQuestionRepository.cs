39b49fe5d6ee1cfc98a5bc9fe5bd1c561b801303
Thomas Schjerlund <thomas@u-man.dk>
2026-08-18 13:05:48 +0200

    Deletes spacing in files

--- Core/Repositories/IQuestionRepository.cs
+++ Core/Repositories/IQuestionRepository.cs
@@ -1,19 +1,10 @@
 ﻿using UCL.QuizApp.Core;
-
 namespace WpfApp1.Core.Repositories
-
 {
-
     public interface IQuestionRepository
-
     {
-
         IEnumerable<Question> GetAll();
-
         Question GetById(int id);
-
         void Add(Question question);
-
     }
-
 }
--- UI/ViewModels/RelayCommand.cs
+++ UI/ViewModels/RelayCommand.cs
@@ -11,32 +11,23 @@ namespace WpfApp1.UI.ViewModels
         private readonly Action<object> _execute;
         private readonly Func<object, bool> _canExecute;
 
-
         public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
-
         {
-            _execute = execute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
 
         }
-
-
         public bool CanExecute(object parameter)
         {
-
             return _canExecute == null || _canExecute(parameter);
         }
 
 
         public void Execute(object parameter)
         {
-
             _execute(parameter);
         }
 
         public event EventHandler CanExecuteChanged; 
-           
-
-        
     }
 }
